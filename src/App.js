3bc05f721641c3afadb676481cb8971597bc7976
nuriapalleja <npalleal40@alumnes.ub.edu>
2024-10-21 12:53:56 +0200

    Drawer creat, falta linkejar Home, Discovery i Saved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
+import Drawer from './components/Drawer';
+import HomePage from './pages/HomePage';
+import DiscoveryPage from './pages/DiscoveryPage';
+import SavedPage from './pages/SavedPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React, this is a test
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <Drawer />
+                </header>
+                <Routes>
+                    <Route path="/home" exact component={HomePage} />
+                    <Route path="/discovery" component={DiscoveryPage} />
+                    <Route path="/saved" component={SavedPage} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
