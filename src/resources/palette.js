ffe7429cefc79f89aeea326101eb67a200a177ea
Miriam Martinez <miriam.martinez.mc@gmail.com>
2024-10-27 05:42:49 +0100

    add need it colors to palette

--- src/resources/palette.js
+++ src/resources/palette.js
@@ -1,5 +1,7 @@
 const paletteColors = {
     background_color: 'linear-gradient(135deg, #282c34, #3a3f47)',
+    textColorStrong: '#7E7E87',
+    textColorWeakest: '#FAF9F6',
     textColor: '#fff',
     primary: '#3498db',
     secondary: '#9b59b6',
