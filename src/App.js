33e023b048cf95cbf6d23a2700ea7f8af9d55708
Miriam Martinez <miriam.martinez.mc@gmail.com>
2024-10-27 03:45:50 +0100

    fix navigation

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
+// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet, useLocation } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import DiscoveryPage from './pages/DiscoveryPage';
 import SavedPage from './pages/SavedPage';
-import Drawer from "./components/Drawer";
+import PermanentDrawer from "./components/Drawer";
 import LandingPage from "./pages/LandingPage";
 
-function AppContent() {
-    const location = useLocation();
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
-            </Routes>
-            <div className="App-header">
-                {location.pathname !== "/" && <Drawer />}
-                <div className="page-content">
-                    <Routes>
-                        <Route path="/home" element={<HomePage />} />
-                        <Route path="/discovery" element={<DiscoveryPage />} />
-                        <Route path="/saved" element={<SavedPage />} />
-                    </Routes>
-                </div>
+// Layout without Drawer (for LandingPage)
+const MainLayout = () => (
+    <div className="App">
+        <Outlet />
+    </div>
+);
+
+// Layout with Drawer (for Home, Discovery, Saved)
+const DrawerLayout = () => (
+    <div className="App">
+        <div className="App-header">
+            <PermanentDrawer isLogged={true} />
+            <div className="page-content">
+                <Outlet />
             </div>
         </div>
+    </div>
+);
+
+function AppContent() {
+    return (
+        <Routes>
+            {/* Routes without Drawer */}
+            <Route element={<MainLayout />}>
+                <Route path="/" element={<LandingPage />} />
+            </Route>
+
+            {/* Routes with Drawer */}
+            <Route element={<DrawerLayout />}>
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/discovery" element={<DiscoveryPage />} />
+                <Route path="/saved" element={<SavedPage />} />
+            </Route>
+        </Routes>
     );
 }
 
