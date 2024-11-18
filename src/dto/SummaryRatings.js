f80b38e8b3aeb83077c27d9204b8599549e47c46
Miriam Martinez <miriam.martinez.mc@gmail.com>
2024-11-19 00:37:42 +0100

    fix typo

--- src/dto/SummaryRatings.js
+++ src/dto/SummaryRatings.js
@@ -1,19 +1,19 @@
 class SummaryRatings {
-    constructor(fiveStarts = 0, fourStarts = 0, threeStarts = 0, twoStarts = 0, oneStarts = 0) {
-        this.fiveStarts = fiveStarts;
-        this.fourStarts = fourStarts;
-        this.threeStarts = threeStarts;
-        this.twoStarts = twoStarts;
-        this.oneStarts = oneStarts;
+    constructor(fiveStars = 0, fourStars = 0, threeStars = 0, twoStars = 0, oneStars = 0) {
+        this.fiveStars = fiveStars;
+        this.fourStars = fourStars;
+        this.threeStars = threeStars;
+        this.twoStars = twoStars;
+        this.oneStars = oneStars;
     }
 
     static fromJSON(json) {
         return new SummaryRatings(
-            json.five_starts || 0,
-            json.four_starts || 0,
-            json.three_starts || 0,
-            json.two_starts || 0,
-            json.one_starts || 0
+            json.five_stars || 0,
+            json.four_stars || 0,
+            json.three_stars || 0,
+            json.two_stars || 0,
+            json.one_stars || 0
         );
     }
 }
