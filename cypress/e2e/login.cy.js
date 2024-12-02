d13208a1aada741d84a7e9b5957d9e0d826b4480
RaulAsins <rasinshi12@alumnes.ub.edu>
2024-12-02 18:10:05 +0100

    update login e2e test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,4 +4,21 @@ describe('Login test', () => {
 
       cy.contains('Welcome').should('be.visible');
   })
+
+  it('should log in through the modal', () => {
+    // Visitar la página principal
+    cy.visit('/'); // Cambia la URL si no es `/`
+
+    // Abrir el modal (asegúrate de que el evento para abrir el modal sea detectable)
+    cy.contains('Login').click(); // Cambia el selector si es necesario
+
+    // Asegúrate de que el modal esté visible
+    cy.get('[aria-labelledby="modal-title"]').should('be.visible');
+
+    // Rellena los campos de usuario y contraseña
+    
+
+
+
+  });
 })
