06c9a324393995096c52ea7faef06c41deeb7f4b
RaulAsins <rasinshi12@alumnes.ub.edu>
2024-12-14 09:54:22 +0100

    api change

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'https://booksocial-dev-development.up.railway.app/'
+    baseURL: process.env.REACT_APP_API_URL || 'https://book-social-backend-production-v2.up.railway.app/'
 });
 
 api.interceptors.request.use(config => {
