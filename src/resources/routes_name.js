fb2a272aeb8a9556290d1079acc7e8b4d48b784e
Miriam Martinez <miriam.martinez.mc@gmail.com>
2024-12-17 00:13:06 +0100

    add saved books in routes

--- src/resources/routes_name.js
+++ src/resources/routes_name.js
@@ -1,8 +1,8 @@
 export const HOME = '/home';
 export const DISCOVERY = '/discovery';
-export const SAVED = '/saved';
+export const SAVED = '/saved-books';
 export const PROFILE = '/profile';
 export const LANDING = '/';
 export const NEW_BOOK = '/new-book';
 export const BOOK_DETAILS = '/book-details/';
-export const NEW_POST ='/new-post';
+export const NEW_POST = '/new-post';
